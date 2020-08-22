ad0ba5dbc4760d22598140bd4d151da94819d653
Anderson Ferreira da Silva <Anderson.silva@peformait.com>
2020-08-22 18:46:43 -0300

    commit_initial

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const server = express();
+const nunjucks = require('nunjucks');
+
+nunjucks.configure('src/views', {
+    express: server,
+    noCache:true
+});
+
+
+server
+
+.use(express.urlencoded({ extended: true}))
+
+.use(express.static('public'))
+
+.get('/', (req, res) => {
+    res.render('home.html');
+})
+
+.get('/home', (req, res) => {
+    res.render('home.html');
+})
+
+.get('/organization', (req, res) => {
+    res.render('organization.html');
+})
+
+.get('/projects', (req, res) => {
+    res.render('projects.html');
+})
+
+.get('/contact', (req, res) => {
+    res.render('contact.html');
+})
+
+.listen('5200');
